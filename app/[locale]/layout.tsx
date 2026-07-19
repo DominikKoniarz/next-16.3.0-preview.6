d9e5093ff5ecabe0ba1ff681e545117e5867228c
DominikKoniarz <dominik.koniarz.development@gmail.com>
2026-07-19 12:47:45 +0200

    Use root param in html lang

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -30,7 +30,7 @@ export default async function RootLayout({
 }>) {
     return (
         <html
-            lang="en"
+            lang={await locale()}
             className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
         >
             <body className="min-h-full flex flex-col">
